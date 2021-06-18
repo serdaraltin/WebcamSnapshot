8d335110f6a85334e87cfdaf1520fb984c234257
serdaraltin <meyta.tr@gmail.com>
2021-06-18 22:43:47 +0300

    upload

---
+++ webcam/Form1.Designer.vb
@@ -0,0 +1,103 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.pView = New System.Windows.Forms.PictureBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.sfdImages = New System.Windows.Forms.SaveFileDialog()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.pView, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(201, 498)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'pView
+        '
+        Me.pView.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.pView.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.pView.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pView.Location = New System.Drawing.Point(12, 12)
+        Me.pView.Name = "pView"
+        Me.pView.Size = New System.Drawing.Size(640, 480)
+        Me.pView.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pView.TabIndex = 4
+        Me.pView.TabStop = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(282, 498)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Stop"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(363, 498)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Save"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(661, 528)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.pView)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        CType(Me.pView, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents pView As PictureBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents sfdImages As SaveFileDialog
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ webcam/Form1.vb
@@ -0,0 +1,76 @@
+﻿
+Imports System.Runtime
+Imports System.Runtime.InteropServices
+Public Class Form1
+    Public hHwnd As Integer
+    Dim yol As String
+    Dim shot As New Threading.Thread(New Threading.ThreadStart(AddressOf aa))
+    Public Declare Function SendMessage Lib "user32" Alias "SendMessageA" _
+    (ByVal hwnd As Integer, ByVal wMsg As Integer, ByVal wParam As Integer, <MarshalAs(UnmanagedType.AsAny)> ByVal lParam As Object) As Integer
+    Public Declare Function SetWindowPos Lib "user32" Alias "SetWindowPos" (ByVal hwnd As Integer, ByVal hWndInsertAfter As Integer, ByVal x As Integer, ByVal y As Integer, ByVal cx As Integer, ByVal cy As Integer, ByVal wFlags As Integer) As Integer
+    Public Declare Function DestroyWindow Lib "user32" (ByVal hndw As Integer) As Boolean
+    Public Declare Function capCreateCaptureWindowA Lib "avicap32.dll" _
+    (ByVal lpszWindowName As String, ByVal dwStyle As Integer, ByVal x As Integer, ByVal y As Integer, ByVal nWidth As Integer, ByVal nHeight As Short, ByVal hWndParent As Integer, ByVal nID As Integer) As Integer
+    Public Declare Function capGetDriverDescriptionA Lib "avicap32.dll" (ByVal wDriver As Short, ByVal lpszName As String, ByVal cbName As Integer, ByVal lpszVer As String, ByVal cbVer As Integer) As Boolean
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        On Error Resume Next
+        Dim random As Random = New Random
+        Dim picture As PictureBox = New PictureBox
+        picture.Size = New Drawing.Size(1280, 720)
+        hHwnd = capCreateCaptureWindowA(0, &H10000000 Or &H40000000, 0, 0, 1280, 720, picture.Handle.ToInt32, 0)
+        If SendMessage(hHwnd, &H400S + 10, 0, 0) Then
+            SendMessage(hHwnd, &H400S + 52, 66, 0)
+        Else
+            DestroyWindow(hHwnd)
+        End If
+        Dim data As IDataObject
+        Dim bmap As Image
+        SendMessage(hHwnd, &H400S + 30, 0, 0)
+        data = Clipboard.GetDataObject()
+        If data.GetDataPresent(GetType(System.Drawing.Bitmap)) Then
+            bmap = CType(data.GetData(GetType(System.Drawing.Bitmap)), Image)
+            SendMessage(hHwnd, &H400S + 11, 0, 0)
+            DestroyWindow(hHwnd)
+            yol = My.Computer.FileSystem.GetTempFileName.ToString()
+            bmap.Save(yol, Imaging.ImageFormat.Jpeg)
+
+        End If
+        data = Nothing
+        pView.Image = Image.FromFile(yol)
+    End Sub
+    Private Sub aa()
+        While (True)
+            Dim random As Random = New Random
+        Dim picture As PictureBox = New PictureBox
+        picture.Size = New Drawing.Size(1280, 720)
+        hHwnd = capCreateCaptureWindowA(0, &H10000000 Or &H40000000, 0, 0, 1280, 720, picture.Handle.ToInt32, 0)
+        If SendMessage(hHwnd, &H400S + 10, 0, 0) Then
+            SendMessage(hHwnd, &H400S + 52, 66, 0)
+        Else
+            DestroyWindow(hHwnd)
+        End If
+        Dim data As IDataObject
+        Dim bmap As Image
+        SendMessage(hHwnd, &H400S + 30, 0, 0)
+        data = Clipboard.GetDataObject()
+        If data.GetDataPresent(GetType(System.Drawing.Bitmap)) Then
+            bmap = CType(data.GetData(GetType(System.Drawing.Bitmap)), Image)
+            SendMessage(hHwnd, &H400S + 11, 0, 0)
+            DestroyWindow(hHwnd)
+            yol = My.Computer.FileSystem.GetTempFileName.ToString()
+            bmap.Save(yol, Imaging.ImageFormat.Jpeg)
+
+        End If
+        data = Nothing
+            pView.Image = Image.FromFile(yol)
+            Threading.Thread.Sleep(3000)
+        End While
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
